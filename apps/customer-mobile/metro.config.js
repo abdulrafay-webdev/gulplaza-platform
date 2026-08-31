a19911e60847972eaa370c2eb2626fa4490d8437
Abdul Rafay <abdullrrafay@gmail.com>
2026-08-31 12:41:12 +0500

    fix(mobile): configure Metro bundler resolveRequest for pre-compiled react-native-svg JS

---
+++ apps/customer-mobile/metro.config.js
@@ -0,0 +1,16 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'react-native-svg') {
+    return {
+      filePath: path.resolve(__dirname, 'node_modules/react-native-svg/lib/module/index.js'),
+      type: 'sourceFile',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
